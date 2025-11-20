11775fa68e8b42b9f8e9f9191a461d2766d241bf
reedmartin <reedmartin10@gmail.com>
2025-11-20 15:42:19 -0800

    these were tricky but got exercise 6 finished!

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    else {
+    let i = 1;
+    while (i <= num) {
+        result += string;
+        i++;
+    }
+        return result;
+    }
 };
 
 // Do not edit below this line
