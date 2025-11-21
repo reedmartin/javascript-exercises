fe40ad44fefc2273b0da447dec87e07d5da0d29f
reedmartin <reedmartin10@gmail.com>
2025-11-21 11:28:46 -0800

    Finished exercise 9!

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (initialA, initialB) {
+    let sum = 0;
+    if ((initialA < 0 || initialB < 0) || (!Number.isInteger(initialA)) || (!Number.isInteger(initialB)) ) {
+        return 'ERROR';
+    }
+    else if (initialA >= initialB) {
+        for (; initialB <= initialA; initialB++) {
+            sum += initialB;
+        }
+    }
+    else {
+        for (; initialA <= initialB; initialA++) {
+            sum += initialA;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
