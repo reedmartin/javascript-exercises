f0ca8a11dd8f99f2e7aa79e67b8781e30235ed9c
reedmartin <reedmartin10@gmail.com>
2025-11-21 08:37:27 -0800

    exercise 7 done!

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    // I can make it an array reverse the order
+    // and join it back together as a string
+    // first take the string and make it an array
+    let initalString = string
+    let initalArray = initalString.split("");
+    // reverse the array
+    let reverseArray = initalArray.reverse();
+    // rejoin the array
+    let rejoinArrary = reverseArray.join("");
+    // return the rejoined array
+    return rejoinArrary;
+    //// I WAS HERE
 };
 
 // Do not edit below this line
